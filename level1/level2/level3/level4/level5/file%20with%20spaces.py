18e4f3ebd6c26a24563fe347472a02a8d5c545c4
Alexandru-Gabriel Huzdup <firstname.lastname@crowdstrike.com>
2026-01-22 18:53:09 +0200

    file name containing special chars

---
+++ level1/level2/level3/level4/level5/file%20with%20spaces.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Hello World in Python
+def main():
+    print("Hello, World!")
+    print("Today is January 19, 2026")
+    print("Welcome to CrowdStrike scripting examples")
+
+if __name__ == "__main__":
+    main()
