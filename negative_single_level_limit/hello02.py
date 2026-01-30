cf8fc1e03da664cf7a34284c61564c747bd7b98f
Alexandru-Gabriel Huzdup <firstname.lastname@crowdstrike.com>
2026-01-30 11:06:34 +0200

    add negative single level limit new directory

---
+++ negative_single_level_limit/hello02.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    """
+    A simple function that prints 'Hello, World!' to the console
+    """
+    print("Hello, World!")
+    
+    # You can add more functionality here 2
+    name = "CrowdStrike"
+    print(f"Hello, {name}! Today is January 19, 2026.")
+
+if __name__ == "__main__":
+    # This ensures the main() function runs only when the script is executed directly
+    # (not when imported as a module)
+    main()
